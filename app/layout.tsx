d80f525f278d98b99d17549d22692bc7acd872c7
tom-walker <walkertom1996@gmail.com>
2024-03-19 01:31:53 +0000

    radix added. initial code updated to radix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Theme accentColor="yellow" panelBackground="solid">{children}</Theme>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { Flex } from '@radix-ui/themes';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-10'>
-      <section className='flex flex-col max-w-3xl w-full items-center justify-center'>
+      <Flex
+        direction='column'
+        width='100%'
+        align='center'
+        justify='center'
+        className='max-w-3xl'
+      >
         <Image
           src='/images/pokemon-logo-500.svg'
           alt='Pokemon logo'
           width={500}
           height={189}
         />
-      </section>
+      </Flex>
     </main>
   );
 }
